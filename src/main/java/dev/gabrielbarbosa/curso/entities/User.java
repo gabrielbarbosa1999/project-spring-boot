c6152d6b59258becb09c32c12938ad1dbf0af31e
Gabriel Barbosa <gabriel.barbosa@myorgtech.com>
2022-08-03 10:34:32 -0300

    H2 database, test profile, JPA

--- src/main/java/dev/gabrielbarbosa/curso/entities/User.java
+++ src/main/java/dev/gabrielbarbosa/curso/entities/User.java
@@ -1,8 +1,16 @@
 package dev.gabrielbarbosa.curso.entities;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.io.Serializable;
+
+@Entity
 public class User implements Serializable {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String email;
